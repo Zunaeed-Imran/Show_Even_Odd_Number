27c8c31ccbca1533329abaeea833c84880ec0e8b
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-04-30 12:25:16 +0600

    Show_Even_Odd_Number in JavaScript

---
+++ show_Even_odd.js
@@ -0,0 +1,12 @@
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 85, 96, 36, 54, 12, 57, 22];
+
+for (let i = 0; i < array[i]; i++){
+  if (array[i] % 2 == 0) {
+    document.write("Number is Even = " + array[i] +"</br>");
+  } else {
+    document.write("Number is odd = " + array[i] + "</br>");
+  }
+}
+
+
+
