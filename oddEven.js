d40fe037042b4e1935e8a31e1421be67fcfd80ae
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-05-04 11:15:12 +0600

    without for loop one number will show
    Show_Even_Odd_Number in JavaScript only one number type will know without for loop

---
+++ oddEven.js
@@ -0,0 +1,9 @@
+let num = [65,224,1,2,59,87,236,22,55,44,858];
+
+  if (num % 2 == 0) {
+    document.write(num + " is Even "+"<br>");
+  } else {
+    document.write(num + " is Odd "+"<br>");
+  }
+
+
